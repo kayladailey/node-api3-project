d15e3d501a5ba52f7ae3bdffe50e4c821eff7826
Kayla Dailey <kayladailey000@gmail.com>
2020-03-19 20:47:33 -0500

    validate user and root route complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code away!
+//Imports
+const express = require ('express');
+const server = express();
+const userRoutes = require('./posts/postRoutes');
+
+//Telling Express to use json for response
+server.use(express.json());
+// base url for our CRUD Routes in the pstRoutes folder
+server.use ('/:id', userRoutes);
+
+//Port we want to see our application on 
+server.listen(8000, () => console.log('API running on port 8000'));
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const router = express.Router();
-
-
-
+const users = require("../data/")
 
 
 router.post('/', (req, res) => {
-  // do your magic!
+  users
+  .find() 
+  //Looking for posts 
+  .then(arr => {
+       // If found YES!
+      res.status(200).json(arr);
+  })
+  .catch(err => {
+      //If not found
+      res.status(500).json({
+          success: false,
+          errorMessage: "Error retrieving posts"
+      });
+  });
 });
 
-router.post('/:id/posts', validateUserId, (req, res) => {
+router.post('/:id/posts', validateUser, (req, res) => {
   // do your magic!
 });
 
@@ -30,7 +41,7 @@ router.delete('/:id',validateUserId, (req, res) => {
 });
 
 router.put('/:id', validateUserId, (req, res) => {
-  // do your magic!
+ 
 });
 
 //custom middleware
@@ -38,7 +49,7 @@ router.put('/:id', validateUserId, (req, res) => {
 function validateUserId (req, res, next) {
   const { id } = req.user
   if (req.url === '/:id' || '/:id/posts' ) {
-    next();
+    next(id);
   } else {
     res.status(404).json({
       success: false,
@@ -48,11 +59,20 @@ function validateUserId (req, res, next) {
 }
 
 function validateUser(req, res, next) {
+    const body = req.body;
+    !body || body === {} ?
+      res.status(400).json({ message: 'Please include request body' })
+      :
+      next();
+  }
 
-}
 
 function validatePost(req, res, next) {
-  // do your magic!
+  const body = req.body;
+  !body || body === {} ?
+    res.status(400).json({ message: "missing user data"})
+    :
+    next();
 }
 
 module.exports = router;
